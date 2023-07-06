ba93c6977aa905f9f2a3d7b425b721d94ab6ea49
EvanderCorvus <3141514142@protonmail.com>
2023-07-06 21:00:43 +0200

    first commit

---
+++ environments.py
@@ -0,0 +1,62 @@
+import numpy as np
+
+class BoxEnvironment1():
+    def __init__(self, space, goal):
+        self.space = space
+        self.goal = goal
+    
+    def init_state(self, agent_batch_size, state_dim):
+        self.agent_batch_size = agent_batch_size
+        self.state = np.zeros((agent_batch_size,state_dim))
+        self.state[:,0] = -0.5*np.ones(agent_batch_size)
+
+    def update_goal(self, goal):
+        self.goal = goal
+    
+    def step(self, action, U0, dt, goal, characteristic_length = 1):
+        x, y = self.state[:,0], self.state[:,1]
+        theta = action[:,0]
+        #thermal noise
+        noise = np.random.normal(np.zeros(self.agent_batch_size),np.ones(self.agent_batch_size)*np.sqrt(dt))
+        # raise Exception(theta.shape, noise.shape)
+        theta = theta + np.sqrt(dt)*characteristic_length*noise
+
+        e_x = np.cos(theta)
+        v_x = e_x 
+        x_new = x + v_x*dt
+
+        e_y = np.sin(theta)
+        v_y = e_y 
+        y_new = y + v_y*dt
+        inside_space = self.space.contains(np.array([x_new, y_new]).T)
+        concentration = compute_concentration(np.array([x_new, y_new]).T, self.goal.center, U0)
+
+        self.state[:,0][inside_space] = x_new[inside_space]
+        self.state[:,1][inside_space] = y_new[inside_space]
+        self.state[:,2] = concentration 
+        
+        # Compute reward
+        reward = self.reward(dt, np.array(inside_space).astype(int), self.state[:,0:2])
+
+        return reward
+    
+    def reward(self, dt, inside_space, position):
+        # Compute reward
+        not_inside_space = np.logical_not(inside_space)
+        reward = -dt*np.ones(self.state.shape[0])
+        wincondition = np.array(self.goal_check()).astype(int)
+        reward += wincondition*1000
+        reward -= not_inside_space*10
+        # reward -= np.linalg.norm(position-self.goal.center, axis=1)*10
+
+        return reward
+    
+    def goal_check(self):
+        position = self.state[:, 0:2]
+        wincondition = self.goal.contains(position)
+        return wincondition
+
+
+def compute_concentration(position, center, U0):
+    r = np.linalg.norm(position-center, axis=1)
+    return U0/(1+r)
---
+++ plot_utils.py
@@ -0,0 +1,147 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import os
+import shutil
+from matplotlib import patches
+
+class RLPlotter():
+    def __init__(self, logger, goal_area):
+        self.logger = logger
+        self.goal_area = goal_area
+
+    def clear_plots(self):
+        clear_folder('episode_losses_critic')
+        clear_folder('episode_losses_actor')
+        clear_folder('episode_actions')
+        clear_folder('episode_paths')
+        clear_folder('run_losses_critic')
+        clear_folder('run_losses_actor')
+        clear_folder('episode_steps')
+        clear_folder('episode_actions_polar')
+
+    def plot_last_episode(self):
+        self.plot_last_episode_losses_critic()
+        self.plot_last_episode_losses_actor()
+        self.plot_last_episode_actions()
+        self.plot_last_episode_paths()
+        self.plot_last_losses_critic()
+        self.plot_last_losses_actor()
+        self.plot_last_episode_steps()
+        self.plot_last_episdode_actions_polar()
+
+    def plot_last_episode_losses_critic(self):        
+        folder = os.path.join('logs', 'episode_losses_critic')
+        i = len(self.logger.episode_losses_critic)-1
+        x = np.arange(len(self.logger.episode_losses_critic[i]))
+        plt.plot(x, self.logger.episode_losses_critic[i])
+        plt.xlabel('Step')
+        plt.ylabel('Loss Critic')
+        plt.title(f'Episode {i} MSE')
+        plt.savefig(os.path.join(folder, f'episode{i}_losses_critic.png'))
+        plt.close()
+    
+    def plot_last_episode_losses_actor(self):
+        folder = os.path.join('logs', 'episode_losses_actor')
+        i = len(self.logger.episode_losses_actor)-1
+        x = np.arange(len(self.logger.episode_losses_actor[i]))
+        plt.plot(x, self.logger.episode_losses_actor[i])
+        plt.xlabel('Step')
+        plt.ylabel('Loss Actor')
+        plt.title(f'Episode {i} MSE')
+        plt.savefig(os.path.join(folder, f'episode{i}_losses_actor.png'))
+        plt.close()
+
+    def plot_last_losses_critic(self):
+        folder = os.path.join('logs', 'run_losses_critic')
+        shutil.rmtree(folder, ignore_errors=True)
+        os.makedirs(folder, exist_ok=True)
+        x = np.arange(len(self.logger.losses_critic))
+        plt.plot(x, self.logger.losses_critic)
+        plt.xlabel('Step')
+        plt.ylabel('Loss Critic')
+        plt.title('Run MSE')
+        plt.savefig(os.path.join(folder, 'run_losses_critic.png'))
+        plt.close()
+    
+    def plot_last_losses_actor(self):
+        folder = os.path.join('logs', 'run_losses_actor')
+        shutil.rmtree(folder, ignore_errors=True)
+        os.makedirs(folder, exist_ok=True)
+        x = np.arange(len(self.logger.losses_actor))
+        plt.plot(x, self.logger.losses_actor)
+        plt.xlabel('Step')
+        plt.ylabel('Loss Actor')
+        plt.title('Run MSE')
+        plt.savefig(os.path.join(folder, 'run_losses_actor.png'))
+        plt.close()
+
+    def plot_last_episode_steps(self):
+        folder = os.path.join('logs', 'episode_steps')
+        shutil.rmtree(folder, ignore_errors=True)
+        os.makedirs(folder, exist_ok=True)
+        x = np.arange(len(self.logger.episode_steps))
+        plt.plot(x, self.logger.episode_steps)
+        plt.xlabel('Episode')
+        plt.ylabel('Steps')
+        plt.savefig(os.path.join(folder, 'episode_steps.png'))
+        plt.close()
+    
+    def plot_last_episode_actions(self):
+        folder = os.path.join('logs', 'episode_actions')
+        i = len(self.logger.episode_actions) - 1
+        x = np.arange(len(self.logger.episode_actions[i]))
+        plt.plot(x, self.logger.episode_actions[i])
+        plt.xlabel('Step')
+        plt.ylabel(r'Active Orientation $\theta$')
+        plt.ylim(0,7)
+        plt.savefig(os.path.join(folder, f'episode{i}_actions.png'))
+        plt.close()
+    
+    def plot_last_episdode_actions_polar(self):
+        folder = os.path.join('logs', 'episode_actions_polar')
+        i = len(self.logger.episode_actions) - 1
+        x = np.arange(len(self.logger.episode_actions[i]))
+        fig = plt.figure()
+        ax = plt.subplot(111, polar=True)
+        ax.scatter(self.logger.episode_actions[i], x, c=x, cmap = 'plasma')
+        fig.savefig(os.path.join(folder, f'episode{i}_actions_polar.png'))
+        plt.close()
+
+    def plot_last_episode_paths(self):
+        folder = os.path.join('logs', 'episode_paths')
+        i = len(self.logger.episode_states) - 1
+        # x = np.array(self.logger.episode_states[i])[:,0,0]
+        # y = np.array(self.logger.episode_states[i])[:,0,1]
+        x = np.array(self.logger.episode_states[i])[:,:,0]
+        y = np.array(self.logger.episode_states[i])[:,:,1]
+        plot_normalized_concentration(self.goal_area)
+        
+        colormap_array = np.linspace(0,1,x.shape[0])
+        for j in range(x.shape[1]):
+            plt.scatter(x[:,j], y[:,j], c=colormap_array, cmap='RdBu', alpha=0.5)
+        # plt.plot(x, y)
+        plt.xlabel('x')
+        plt.ylabel('y')
+        plt.title(f'Episode {i} Path')
+        plt.savefig(os.path.join(folder, f'episode{i}_path.png'))
+        plt.close()
+
+def plot_normalized_concentration(goal_area):
+    x,y = np.meshgrid(np.linspace(-1,1,100),np.linspace(-1,1,100))
+    r = np.sqrt((x-goal_area.centerX[0])**2+(y-goal_area.centerY[0])**2)
+    concentration = 1/(1+r)
+
+    plt.xlim(-1,1)
+    plt.ylim(-1,1)
+    # patches.Rectangle((goal_area.centerX-goal_area.width/2, goal_area.centerY-goal_area.height/2), goal_area.width, goal_area.height, color='black', fill=False, alpha=0.5, label='goal area')
+    plt.imshow(concentration[:,],cmap = 'plasma',extent=[-1,1,-1,1],origin='lower')
+    colorbar = plt.colorbar()
+    colorbar.set_label(r'$c/c_0$',labelpad=10,fontsize = 20)
+
+
+def clear_folder(folder_name):
+    folder = os.path.join('logs', folder_name)
+    shutil.rmtree(folder, ignore_errors=True)
+    os.makedirs(folder, exist_ok=True)
+
+
